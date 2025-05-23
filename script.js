3ea17bb39ca677890a52796a800f982b7378eb87
Trey-Farner <trey.farner@mscoding.org>
2025-05-23 13:29:59 -0500

    Medium level started

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const container = document.getElementById("game-container")
 const loadScreen = document.getElementById("title-login")
 const resetBtn = document.getElementById("reset-btn")
 const menuBtn = document.getElementById("menu-btn")
+const mediumBtn = document.getElementById("medium-btn")
+const mediumGrid = document.getElementById("med-container")
 let answer = Math.floor(Math.random() * 8) + 1
 
 container.style.display = "none"
@@ -38,3 +40,31 @@ resetBtn.addEventListener("click", () => {
     answer = Math.floor(Math.random() * 8) + 1
 })
 menuBtn.addEventListener("click", () => window.location.reload())
+
+//Medium Level code-- I have to generate the grid here
+
+let medGrid = []
+let row = 5
+let col = 5
+
+mediumBtn.addEventListener("click", () => {
+    let val = 0
+    loadScreen.style.display = "none"
+    // debugger
+    for (let i = 0; i <= 4; i++) {
+        medGrid[i] = []
+        for (let j = 0; j <= 4; j++) {
+            medGrid[i][j] = j
+            const tile = document.createElement("div")
+            mediumGrid.appendChild(tile)
+            tile.id = `tile-${i}-${j}`
+            tile.classList.add("med-tiles")
+        }
+    }
+    mediumGrid.style.display = "grid"
+    console.log(medGrid)
+})
+
+tile.addEventListener("click", () => {
+    
+})
